eb834e7f5acb41c3a30e1463e295f989bbdb0a66
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2020-08-06 22:44:14 -0300

    arrays

---
+++ array-slice-map/array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Declaration 1
+	//var arr = [5]int{1, 2, 3, 4, 5}
+	var arr [5]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	arr[3] = 4
+	arr[4] = 5
+	//arr := [5]int{1,2,3,4,5}
+
+	fmt.Println(arr)
+}
