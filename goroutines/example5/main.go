be692d82b4a4b2c6219638f4a726a5886f1ae229
Fabio Azevedo <emaildofabioeduardo@gmail.com>
2024-05-13 23:53:59 -0300

    update

---
+++ goroutines/example5/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	s := []int{10, 23, 33, 42, 55, 59}
+
+	c := make(chan int)
+
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+
+	x, y := <-c, <-c
+
+	fmt.Printf("%d + %d = %d\n", x, y, x+y)
+}
